e68c968b7c1965121f4efb98065673e69eaf5e0e
Kalpas397 <110771132+Kalpas397@users.noreply.github.com>
2025-03-10 17:52:33 +0900

    Add FruitsGoal
    木のみ納品時のオブジェクトの処理を実装

--- Assets/Codes/PlayerCollision.cs
+++ Assets/Codes/PlayerCollision.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     [SerializeField] private Transform saucer;  // 受け皿オブジェクト
-    // [SerializeField] private Transform[] fruits;
+    [SerializeField] private GameObject[] fruits;    // 拾ったフルーツを格納する配列
     [SerializeField] private int maxFruitCount = 100;
     [SerializeField] private int fruitCount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        fruits = new GameObject[100];
     }
 
     // Update is called once per frame
@@ -23,21 +23,39 @@ public class PlayerCollision : MonoBehaviour
 
     private void OnTriggerEnter(Collider collision)
     {
+        // フルーツに触れた処理
         if (collision.gameObject.CompareTag("Fruit"))
         {
             if (fruitCount < maxFruitCount)
             {
-                fruitCount++;
                 StackFruits(collision.gameObject);
+                fruitCount++;
             }
-            
         }
+
+        // 納品カゴ
+        if (collision.gameObject.CompareTag("Goal"))
+        {
+            if (fruitCount >= 1)
+            {
+                while (fruitCount > 0)
+                {
+                    Debug.Log("fruit: " + fruitCount);
+                    Destroy(fruits[fruitCount - 1]);
+                    fruits[fruitCount - 1] = null;
+                    fruitCount -= 1;
+                }
+            }
+        }
+
+        // ダメージオブジェクト
     }
 
     private void StackFruits(GameObject fruit)
     {
+        fruits[fruitCount] = fruit;
         fruit.transform.SetParent(transform);
-        fruit.transform.localPosition = new Vector3(0, fruitCount + 0.5f, 0);
+        fruit.transform.localPosition = new Vector3(0, fruitCount+ 1.0f + 0.5f, 0);
         fruit.GetComponent<SphereCollider>().enabled = false;
     }
 }
